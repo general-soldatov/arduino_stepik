f4e750df5bf2e7742f2a190193af64e351b12625
Юрий Солдатов <root@DESKTOP-KVTP98R.localdomain>
2026-02-07 20:52:02 +0300

    Set of output

--- template_arduino/led_1.cpp
+++ template_arduino/led_1.cpp
@@ -1,7 +1,8 @@
 // c++
 // header
-#define TEST 3
+#define TEST 1
 #include <iostream>
+#include <iomanip>
 #include <map>
 #include <string>
 
@@ -11,20 +12,26 @@ private:
         {"green", "🟩"}
     };
     std::string color[2] = {"⬛"};
-    unsigned char digit[13] = {0};
-    unsigned char mode[13] = {0};
+    unsigned char console[13][2] = {{0, 0}};
 
 public:
     Led(std::string colors) {
         color[1] = colors_data[colors];
     }
 
-    void pinMode(unsigned char, unsigned char);
-    void digitalWrite(unsigned char, unsigned char);
-    void printPanel() {
-        for (auto led : digit)
-            std::cout << color[led];
-        std::cout << std::endl;
+    void pinMode(unsigned char pin, unsigned char mode) {
+        console[pin][0] = mode;
+    }
+
+    void digitalWrite(unsigned char pin, unsigned char mode) {
+        console[pin][1] = mode;
+    }
+
+    void delay_ms(int ms, int st) {
+        std::cout << "LED | ";
+        for (const auto& [registr, digit] : console)
+            std::cout << color[(registr & digit)];
+        std::cout << " | PAUSE: " << std::setw(st) << ms << " ms |" << std::endl;
     }
 
 };
@@ -36,47 +43,42 @@ public:
 #define HIGH 1
 #define LOW 0
 
-void delay(int ms);
-void pinMode(unsigned char, unsigned char);
-void digitalWrite(unsigned char, unsigned char);
+Led led("green");
+
+void delay(int ms) {
+    led.delay_ms(ms, 5);
+}
+
+void pinMode(unsigned char pin, unsigned char mode) {
+    led.pinMode(pin, mode);
+}
+
+void digitalWrite(unsigned char pin, unsigned char mode) {
+    led.digitalWrite(pin, mode);
+}
 
 // code
 void setup() {
     // put your setup code here, to run once:
+    for (int i = 0; i < 13; i++)
+        pinMode(i, OUTPUT);
 
 }
 
 void loop() {
     // put your main code here, to run repeatedly
+    for (int i = 0; i < 13; i++) {
+        digitalWrite(i, HIGH);
+        delay(1500);
+        digitalWrite(i, LOW);
+    }
 
 }
 
 // footer
 
 int main(int argc, char* argv[]) {
-    // std::string leds[13] = {};
-    // setup();
-    Led led("green");
-    led.printPanel();
-    // for (int i = 0; i < TEST; i++)
-    //     loop();
-    // for (auto &data : leds) {
-    //     data = OFF;
-    // }
-    // std::cout << leds << std::endl;
+    setup();
+    for (int i = 0; i < TEST; i++) loop();
     return 0;
-}
-
-
-
-void delay(int ms) {
-
-}
-
-void pinMode(unsigned char pin, unsigned char mode) {
-
-}
-
-void digitalWrite(unsigned char pin, unsigned char mode) {
-
 }
